dd8ae1db445585037604293de717765113d859df
Fiston <karekezifiston30@gmail.com>
2024-11-08 12:46:55 +0200

    a

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -20,6 +20,35 @@ const AddProduct = () => {
     }
     const add_product = async ()=>{
         console.log(productDetails)
+        let responseData;
+        let product=productDetails;
+
+        let formData= new FormData();
+        formData.append('product',image);
+
+        await fetch('http://localhost:4000/upload',{
+          method:'POST',
+          headers:{
+            Accept:'application/json',
+          },
+          body:formData,
+        }).then((resp)=>resp.json()).then((data)=>{responseData=data});
+
+        if(responseData.success)
+          {
+            product.image=responseData.image_url;
+            console.log(product);
+            await fetch('http://localhost:4000/addproduct',{
+              method:'POST',
+              headers:{
+                Accept:'application/json',
+                'Content-Type':'application/json'
+              },
+              body:JSON.stringify(product),
+            }).then((resp)=>resp.json()).then((data)=>{
+              data.success?alert('Product Added'):alert('Failed')
+            })
+          }
     }
 
 
