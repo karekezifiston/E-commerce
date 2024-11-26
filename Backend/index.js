eadada713a38ef6d0527b9ed2f2d2344bdcfaab3
Fiston <karekezifiston30@gmail.com>
2024-11-26 17:58:45 +0200

    a

--- Backend/index.js
+++ Backend/index.js
@@ -46,7 +46,7 @@ app.post('/upload', upload.single('product'), (req, res) => {
     }
     res.json({
         success: 1,
-        image_url: `https://shop-eco-backend.onrender.com/images/${req.file.filename}`
+        image_url:`https://shop-eco-backend.onrender.com/images/${req.file.filename}`
     });
 });
 
