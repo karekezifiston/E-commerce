3ca2dfb19c81220176480ae0dec5a1482127747d
Fiston <karekezifiston30@gmail.com>
2024-11-26 18:53:25 +0200

    a

--- admin/src/Components/SideBar/SideBar.jsx
+++ admin/src/Components/SideBar/SideBar.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import './SideBar.css'
 import {Link} from 'react-router-dom'
-import add_product_icon from '../../assets/add.PNG'
 import list_product_icon from '../../assets/list.PNG'
+import addImage from '../../assets/add.png';
 
 const SideBar = () => {
   return (
     <div className='sidebar'> 
        <Link to={'/addproduct'} style={{textDecoration:'none'}}>
          <div className="sidebar-item">
-            <img src={add_product_icon} width={60} alt="" />
+            <img src={addImage} width={60} alt="" />
             <p>Add Product</p>
          </div>
     </Link>  
