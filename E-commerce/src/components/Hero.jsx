b773f422e8cd26156384d24977c005ced1c52154
Fiston <karekezifiston30@gmail.com>
2024-12-06 19:36:06 +0200

    a

--- E-commerce/src/components/Hero.jsx
+++ E-commerce/src/components/Hero.jsx
@@ -1,51 +1,38 @@
-import React from 'react'
-import './Hero.css'
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import menTrend from '../assets/mens1.png'
-import womenTrend from '../assets/womens.png'
-import women from '../assets/womens1.png'
-import mens from '../assets/mens2.png'
-import down from '../assets/down.png'
+import React, { useContext, useEffect, useState } from 'react';
+import './Hero.css';
+import { ShopContext } from './Context/ShopContext';
+import Item from './Item/Item'; // Importing the Item component
 
 const Hero = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    speed: 1000,
-    autoplaySpeed: 2550,
-    cssEase: "linear"
-  };
-  return (
-    <div id='hero' >
-      <div className='twoss'>
-      <div className="slider-container">
-        <div className='hero-title'>
-          <h1>New Trends </h1> 
-        </div>
-      <Slider {...settings}>
-        <div>
-        <img src={women} className='yyy' alt="" />
-        </div>
-        <div>
-        <img src={mens} className='yy' alt="" />
-        </div>
-        <div>
-        <img src={menTrend}className='yyy' alt="" />
-        </div>
-        <div>
-        <img src={womenTrend}className='yyy' alt="" />
-        </div>
-      </Slider>
-    </div>
-    <a href="#service"><img className='fg' src={down}width={40} alt="" /></a>
-    </div>
-    </div>
-  )
-}
+  const { all_product } = useContext(ShopContext);
+  const [bestSeller, setBestSeller] = useState([]);
 
-export default Hero
+  useEffect(() => {
+    if (all_product) {
+      setBestSeller(all_product.slice(0, 4)); // Get the first 5 products as bestsellers
+    }
+  }, [all_product]);
+
+  return (
+    <div className="hero">
+      <div className="title-trend">
+        <h1>Our Trends</h1>
+      </div>
+      <div className="product-items">
+        {bestSeller.map((product, index) => (
+          <Item
+            key={index}
+            id={product._id} // Assuming _id is the unique field
+            image={product.image}
+            name={product.name}
+            type={product.type}
+            description={product.description}
+            price={product.price}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
