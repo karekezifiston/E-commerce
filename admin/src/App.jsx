113d548abc274236f6fa0eacc19ed493e6f6483c
Fiston <karekezifiston30@gmail.com>
2024-12-13 20:50:03 +0200

    a

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,29 +1,29 @@
 import React from 'react'
 import Sidebar from './components/Sidebar/Sidebar';
-import {Route, Routes} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Add from './Pages/Add/Add'
 import List from './Pages/List/List'
 import Orders from './Pages/Orders/Orders'
-import { ToastContainer} from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Navbar from './components/Navbar/Navbar'
 
 const App = () => {
 
-  const url ="https://shop-eco-backend.onrender.com";
+  const url = "https://shop-eco-backend.onrender.com";
 
   return (
     <div className='app'>
-       <ToastContainer/>
-      <Navbar/>
+      <ToastContainer />
+      <Navbar />
       <hr />
       <div className='app-content'>
-          <Sidebar/>
-          <Routes>
-            <Route path='/add' element={<Add url={url}/>}/>
-            <Route path='/list' element={<List url={url}/>}/>
-            <Route path='/orders' element={<Orders url={url}/>}/>
-          </Routes>
+        <Sidebar />
+        <Routes>
+          <Route path='/add' element={<Add url={url} />} />
+          <Route path='/list' element={<List url={url} />} />
+          <Route path='/orders' element={<Orders url={url} />} />
+        </Routes>
       </div>
     </div>
   )
