01cf39e3c538e4b68b4390f0a6dd2cd5bd6366d8
Fiston <karekezifiston30@gmail.com>
2024-12-16 16:33:15 +0200

    a

--- Backend/server.js
+++ Backend/server.js
@@ -1,39 +1,36 @@
-import express from "express";
-import cors from "cors";
-import { connectDB } from "./config/db.js";
-import productRouter from "./routes/productRoute.js";
-import userRouter from "./routes/userRoute.js";
-import cartRouter from "./routes/cartRoute.js";
-import 'dotenv/config';
-import fs from 'fs';
+import express from "express"
+import cors from "cors"
+import { connectDB } from "./config/db.js"
+import productRouter from "./routes/productRoute.js"
+import userRouter from "./routes/userRoute.js"
+import 'dotenv/config'
+import cartRouter from "./routes/cartRoute.js"
 
-// Ensure uploads directory exists
-if (!fs.existsSync('uploads')) {
-  fs.mkdirSync('uploads');
-}
 
-// App Config
-const app = express();
-const port = process.env.PORT || 4000;
 
-// Middleware
-app.use(express.json());
-app.use(cors());
-app.use("/images", express.static('uploads'));
 
-// Database Connection
+
+// app config
+const app =express()
+const port =process.env.PORT || 4000
+
+// middleware
+app.use(express.json())
+app.use(cors())
+
+// db connection
 connectDB();
 
-// API Endpoints
-app.use("/api/product",productRouter);
-app.use("/api/user", userRouter);
-app.use("/api/cart", cartRouter);
+// api endpoints
+app.use("/api/product",productRouter)
+app.use("/images",express.static('uploads'))
+app.use("/api/user",userRouter)
+app.use("/api/cart",cartRouter)
 
-app.get("/", (req, res) => {
-  res.send("API Working");
-});
+app.get("/",(req,res)=>{
+  res.send("API Working")
+})
 
-// Start Server
-app.listen(port, () => {
-  console.log(`Server started on http://localhost:${port}`);
-});
+app.listen(port,()=>{
+    console.log(`Server Started on http://localhost:${port}`)
+})
