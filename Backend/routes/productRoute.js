87e64545549378fc1637fa4a9e47aa715a45d4f9
Fiston <karekezifiston30@gmail.com>
2024-12-17 14:04:07 +0200

    a

--- Backend/routes/productRoute.js
+++ Backend/routes/productRoute.js
@@ -17,7 +17,7 @@ const upload =multer({storage:storage})
  
 productRouter.post("/add",upload.single("image"),addProduct)
 productRouter.get("/list",listProduct)
-productRouter.post("/remove",removeProduct);
+foodRouter.post("/remove",removeProduct);
 
 
 
