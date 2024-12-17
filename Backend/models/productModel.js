4307651f69dd6536d73869d6ad0afcd8ad1d7371
Fiston <karekezifiston30@gmail.com>
2024-12-17 12:22:42 +0200

    a

--- Backend/models/productModel.js
+++ Backend/models/productModel.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 
-const productSchema =new mongoose.Schema({
-    name:{type:String,requireed:true},
-    description:{type:String,required:true},
-    price:{type:Number,required:true},
-    image:{type:String,required:true},
-    category:{type:String,required:true}
-})
+const productSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    image: { type: String, required: true },
+    category: { type: String, required: true }
+});
 
-const productModel= mongoose.model.product || mongoose.model("product",productSchema);
+const productModel = mongoose.models.product || mongoose.model("product", productSchema);
 
 export default productModel;
